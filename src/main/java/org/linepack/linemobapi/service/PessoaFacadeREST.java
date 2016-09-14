65360cce939aebf03766cbbd7b3a62f3abc3554e
Leandro <leandro_franciscato@hotmail.com>
2016-09-14 14:43:13 -0300

    Finalizing the Sync of the Updates, Inserts and deletes... continue...

--- src/main/java/org/linepack/linemobapi/service/PessoaFacadeREST.java
+++ src/main/java/org/linepack/linemobapi/service/PessoaFacadeREST.java
@@ -8,8 +8,11 @@ package org.linepack.linemobapi.service;
 import java.net.UnknownHostException;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.linepack.linemobapi.model.Pessoa;
@@ -20,12 +23,12 @@ import org.linepack.linemobapi.model.Pessoa;
  */
 @Stateless
 @Path("pessoa")
-public class PessoaFacadeREST extends AbstractFacade<Pessoa>{
+public class PessoaFacadeREST extends AbstractFacade<Pessoa> {
 
     public PessoaFacadeREST() {
-        super(Pessoa.class);        
+        super(Pessoa.class);
     }
-    
+
     @POST
     @Override
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
@@ -34,8 +37,21 @@ public class PessoaFacadeREST extends AbstractFacade<Pessoa>{
         return super.create(entity);
     }
 
-    
-    
-    
-    
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Override
+    public String remove(@PathParam("id") String id) throws UnknownHostException, IllegalArgumentException, IllegalAccessException {
+        return super.remove(id);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces(MediaType.TEXT_PLAIN)
+    @Override
+    public String edit(@PathParam("id") String id, Pessoa entity) throws UnknownHostException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        return super.edit(id, entity);
+    }
+
 }
